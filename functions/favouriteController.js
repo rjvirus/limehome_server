165c3502d65229563388bd4b893c6de809cf46a3
rjvirus <rahulvirus100@gmail.com>
2021-12-10 01:44:16 +0100

    cleanup routes and add controller

---
+++ functions/favouriteController.js
@@ -0,0 +1,83 @@
+const admin = require("firebase-admin");
+const config = require('./config.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(config.FIREBASE_CONFIG),
+    databaseURL: "https://fir-api-9a206..firebaseio.com"
+});
+
+const db = admin.firestore();
+
+function addFavourite(req, res) {
+	const propertyId = req.body.propertyId;
+	let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
+
+	docRef.get().then(doc => {
+		if (!doc.exists) {
+			res.json({ success: false });
+		} else {
+			return doc.data()
+		}
+	}).then(data => {
+		let newFav = data.favourites.slice();
+		if (data.favourites.indexOf(propertyId) === -1) {
+			newFav.push(propertyId);
+		} else {
+			throw Error('Already exists');
+		}
+		return docRef.update({ favourites: newFav });
+	}).then(doc => {
+		res.json({ success: true });
+	}).catch((e) => {
+		res.json({
+			success: false,
+			message: e.message
+		})
+	});
+}
+
+function deleteFavourite(req, res) {
+	const propertyId = req.body.propertyId;
+	let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
+
+	if (typeof propertyId === 'string') {
+		res.status(400).json({ success: false, message: "Bad Request" })
+	} else {
+		docRef.get().then(doc => {
+			if (!doc.exists) {
+				res.status(500).json({ success: false, message: "Database not initialized" });
+			} else {
+				return doc.data()
+			}
+		}).then(data => {
+			let updatedFav = data.favourites.slice();
+			let index = data.favourites.indexOf(propertyId);
+			if (index === -1) {
+				throw Error('Does not exists');
+			} else {
+				updatedFav.splice(index, 1);
+			}
+			return docRef.update({ favourites: updatedFav });
+		}).then(doc => {
+			res.status(200).json({ success: true });
+		}).catch((e) => {
+			res.status(500).json({
+				success: false,
+				error: e
+			})
+		});
+	}
+}
+
+function getAllFavourite(req, res) {
+  let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
+  docRef.get().then(doc => {
+    if (!doc.exists) {
+      res.json({ message: 'No such document!' });
+    } else {
+      res.json({ message: 'Succesfull!', data: doc.data().favourites });
+    }
+  })
+}
+
+module.exports = { addFavourite, deleteFavourite, getAllFavourite }
--- functions/index.js
+++ functions/index.js
@@ -1,90 +1,14 @@
 const functions = require('firebase-functions');
-const config = require('./config.json');
-const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { addFavourite, deleteFavourite, getAllFavourite } = require('./favouriteController');
 app.use(cors({ origin: true }));
 
-admin.initializeApp({
-  credential: admin.credential.cert(config.FIREBASE_CONFIG),
-  databaseURL: "https://fir-api-9a206..firebaseio.com"
-});
+app.put('/api/favourites', addFavourite);
 
-const db = admin.firestore();
+app.delete('/api/favourites', deleteFavourite)
 
-app.put('/api/favourites', (req, res) => {
-  const propertyId = req.body.propertyId;
-  let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
-
-  docRef.get().then(doc => {
-    if (!doc.exists) {
-      res.json({ success: false });
-    } else {
-      return doc.data()
-    }
-  }).then(data => {
-    let newFav = data.favourites.slice();
-    if (data.favourites.indexOf(propertyId) === -1) {
-      newFav.push(propertyId);
-    } else {
-      throw Error('Already exists');
-    }
-    return docRef.update({ favourites: newFav });
-  }).then(doc => {
-    res.json({ success: true });
-  }).catch((e) => {
-    res.json({
-      success: false,
-      message: e.message
-    })
-  });
-
-
-});
-
-app.delete('/api/favourites', (req, res) => {
-  const propertyId = req.body.propertyId;
-  let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
-
-  if(typeof propertyId === 'string') {
-    res.status(400).json({ success: false, message: "Bad Request" })
-  }
-  docRef.get().then(doc => {
-    if (!doc.exists) {
-      res.status(500).json({ success: false, message: "Database not initialized" });
-    } else {
-      return doc.data()
-    }
-  }).then(data => {
-    let updatedFav = data.favourites.slice();
-    let index = data.favourites.indexOf(propertyId);
-    if (index === -1) {
-      throw Error('Does not exists');
-    } else {
-      updatedFav.splice(index, 1);
-    }
-    return docRef.update({ favourites: updatedFav });
-  }).then(doc => {
-    res.status(200).json({ success: true });
-  }).catch((e) => {
-    res.status(500).json({
-      success: false,
-      error: e
-    })
-  });
-
-})
-
-app.get('/api/favourites', (req, res) => {
-  let docRef = db.collection('users').doc('pCsGr1nIgRT0iPvU76m0');
-  docRef.get().then(doc => {
-    if (!doc.exists) {
-      res.json({ message: 'No such document!' });
-    } else {
-      res.json({ message: 'Succesfull!', data: doc.data().favourites });
-    }
-  })
-});
+app.get('/api/favourites', getAllFavourite);
 
 exports.app = functions.https.onRequest(app);
