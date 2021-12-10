cb7d909a6a62bfbfe4a3e8173d4627c5c95542dd
rjvirus <rahulvirus100@gmail.com>
2021-12-10 01:55:35 +0100

    add comment

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { addFavourite, deleteFavourite, getAllFavourite } = require('./controllers/favourite');
+
+// Middleware
 app.use(cors({ origin: true }));
 
+//Route
 app.put('/api/favourites', addFavourite);
-
 app.delete('/api/favourites', deleteFavourite)
-
 app.get('/api/favourites', getAllFavourite);
 
 exports.app = functions.https.onRequest(app);
